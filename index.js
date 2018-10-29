4545a0f138f876705c74b1ff6053a39fc2d9168a
Andrew <amjjunk@gmail.com>
2018-10-29 12:21:43 -0700

    test with text first

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ ldclient.once('ready', function() {
     }
   });
 });
+var user = {
+  firstName: 'Andrew',
+  lastName: 'Jones',
+  key: 'amjjunk@gmail.com',
+  custom: {
+    groups: 'beta_testers'
+  }
+};
+
+client.once('ready', function() {
+  client.variation('new-image', user, false, function(err, showFeature) {
+    if (showFeature) {
+      // application code to show the feature
+      console.log('Showing your feature to ' + user.key );
+    } else {
+      // the code to run if the feature is off
+      console.log('Not showing your feature to ' + user.key);
+    }
+
+    client.flush(function() {
+      client.close();
+    });
+  });
+});
